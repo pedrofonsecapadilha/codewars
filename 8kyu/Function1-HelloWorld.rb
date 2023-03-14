ce044a3b4966a90b37042be239c207a11f9d5f14
Pedro Padilha <pedrofpadilha@gmail.com>
2023-03-13 21:16:01 -0300

    chalenge

---
+++ 8kyu/Function1-HelloWorld.rb
@@ -0,0 +1,12 @@
+# DESCRIPTION:
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+# My solution
+def greet ()
+  "hello world!"
+end
+
+# Best one
+def greet
+  "hello world!"
+end

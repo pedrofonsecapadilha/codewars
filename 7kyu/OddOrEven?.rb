2ae2a55e70b0eaba06928732ef0f8f63f8cedeba
Pedro Padilha <pedrofpadilha@gmail.com>
2023-03-29 09:54:34 -0300

    challenge

---
+++ 7kyu/OddOrEven?.rb
@@ -0,0 +1,28 @@
+# DESCRIPTION:
+  # Task:
+  # Given a list of integers, determine whether the sum of its elements is odd or even.
+
+  # Give your answer as a string matching "odd" or "even".
+
+  # If the input array is empty consider it as: [0] (array with a zero).
+
+  # Examples:
+  # Input: [0]
+  # Output: "even"
+
+  # Input: [0, 1, 4]
+  # Output: "odd"
+
+  # Input: [0, -1, -5]
+  # Output: "even"
+  # Have fun!
+
+# My solution
+def odd_or_even(array)
+  array.sum % 2 == 0 ? 'even' : 'odd'
+end
+
+# Best one
+def odd_or_even(array)
+  array.sum.even? ? 'even' : 'odd'
+end

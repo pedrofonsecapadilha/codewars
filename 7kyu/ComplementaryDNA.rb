c04324fc86b2d741e3a105cc6b453d008415bd8e
Pedro Padilha <pedrofpadilha@gmail.com>
2023-04-12 18:37:04 -0300

    challenge

---
+++ 7kyu/ComplementaryDNA.rb
@@ -0,0 +1,37 @@
+# DESCRIPTION:
+  # Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the
+  # development and functioning of living organisms.
+
+  # If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+  # In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+  # Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side.
+  # DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+  # More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+  # Example: (input --> output)
+
+  # "ATTGC" --> "TAACG"
+  # "GTAT" --> "CATA"
+
+# My solution
+LIST = {
+  'A' => 'T',
+  'C' => 'G',
+  'T' => 'A',
+  'G' => 'C'
+}
+
+def DNA_strand(dna)
+  result = []
+  dna.split("").each do |i|
+    result << LIST[i]
+  end
+  result.join()
+end
+
+# Best one
+def DNA_strand(dna)
+  dna.tr("ACTG", "TGAC")
+end

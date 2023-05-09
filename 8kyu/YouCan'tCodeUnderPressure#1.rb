71160d4e7bcbc577c154b367126378b9adc34e6a
Pedro Padilha <pedrofpadilha@gmail.com>
2023-05-09 09:02:49 -0300

    challenge

---
+++ 8kyu/YouCan'tCodeUnderPressure#1.rb
@@ -0,0 +1,12 @@
+# DESCRIPTION:
+  # Code as fast as you can! You need to double the integer and return it.
+
+# My solution
+def double_integer(i)
+  i * 2
+end
+
+# Best one
+def doubleInteger(i)
+  i*2
+  end

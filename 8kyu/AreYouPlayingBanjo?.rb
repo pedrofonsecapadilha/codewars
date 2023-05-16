a4a8ca6a6f969bc5f785412f9aaa0c09cab74824
Pedro Padilha <pedrofpadilha@gmail.com>
2023-05-16 15:03:49 -0300

    challenge

---
+++ 8kyu/AreYouPlayingBanjo?.rb
@@ -0,0 +1,23 @@
+# DESCRIPTION:
+  # Create a function which answers the question "Are you playing banjo?".
+  # If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+  # The function takes a name as its only argument, and returns one of the following strings:
+
+  # name + " plays banjo"
+  # name + " does not play banjo"
+  # Names given are always valid strings.
+
+# My solution
+def are_you_playing_banjo(name)
+  if name.downcase.split('')[0] == 'r'
+    return name + " plays banjo"
+  else
+    return name + " does not play banjo"
+  end
+end
+
+# Best one
+def are_you_playing_banjo(name)
+  name[0].downcase == "r" ? "#{name} plays banjo" : "#{name} does not play banjo"
+end

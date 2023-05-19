76f07e3367fa602a8924fb18e3ae8782a89054b6
Pedro Padilha <pedrofpadilha@gmail.com>
2023-05-19 17:25:00 -0300

    challenge

---
+++ 5kyu/-TowersOfHanoi.rb
@@ -0,0 +1,20 @@
+# DESCRIPTION:
+  # Towers of Hanoi is a game where there are 3 pegs, which we will call "A", "B" and "C".
+  # Peg A starts with X number of discs, each disc is smaller than the disc underneith.
+
+  # The game is to move all the discs from A to C, 1 at a time, without ever having a disc on top of a smaller disc.
+
+  # You are given the number of discs initially on peg A, and you must return the shortest list of moves to get all
+  # the discs to C. A move is an array of 2 items, the peg the disc is moving from and the peg it's moving too.
+  # ['A', 'C'] means move from peg A to C.
+
+  # e.g.
+
+  # Hanoi.new(1).moves => [['A', 'C']]
+  # Hanoi.new(2).moves => [['A', 'B'], ['A', 'C'], ['B', 'C']]
+  # The number of discs will be 0 < n < 10
+
+# My solution
+
+
+# Best one

8d9761a10124af515e02fa3e4fa8c62896da7110
Pedro Padilha <pedrofpadilha@gmail.com>
2023-05-29 08:52:48 -0300

    challenge

--- 6kyu/GetIndicesOfItemWeights.rb
+++ 6kyu/GetIndicesOfItemWeights.rb
@@ -19,6 +19,27 @@
   # [output] array.integer
 
 # My solution
-
+# ACHO QUE TEM UM ERRO NOS TESTES - FALA QUE TEVE Failed Tests, Took longer than 5 seconds (Completed in 0.8517ms), O PROBLEMA ACHO QUE ESTÁ EM ACEITAR O INDEX 0 NA RESPOSTA.
+def get_indices_of_item_weights(arr, limit)
+  arr2 = arr - [arr[0]]
+  result = []
+  arr.each_with_index do |e1, i1|
+    if result.size < 1
+      arr2.each_with_index do |e2, i2|
+        if e1 + e2 >= limit
+          result << i1
+          result << (i2+1)
+          break
+        end
+      end
+    else
+      break
+    end
+  end
+  result
+end
 
 # Best one
+def get_indices_of_item_weights(arr, limit)
+  arr.each_index.to_a.combination(2).detect { |a, b| arr[a] + arr[b] == limit }
+end

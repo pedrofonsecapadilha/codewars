959f16ce798b1f2bb26dcd744df0a22f2181170e
Pedro Padilha <pedrofpadilha@gmail.com>
2023-06-02 09:30:59 -0300

    challenge - erro, tem que voltar

--- 3kyu/-MakeASpiral.rb
+++ 3kyu/-MakeASpiral.rb
@@ -28,6 +28,34 @@
   # General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.
 
 # My solution
+def spiralize(size)
+  spiral = Array.new(size) { Array.new(size, 0) }
+  directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  direction_index = 0
+  x = 0
+  y = 0
 
+  (1..size*size).each do |num|
+    spiral[x][y] = 1
+
+    next_x = x + directions[direction_index][0]
+    next_y = y + directions[direction_index][1]
+
+    if (next_x < 0 || next_x >= size || next_y < 0 || next_y >= size || spiral[next_x][next_y] == 1)
+      direction_index = (direction_index + 1) % 4
+      next_x = x + directions[direction_index][0]
+      next_y = y + directions[direction_index][1]
+
+      if (next_x < 0 || next_x >= size || next_y < 0 || next_y >= size || spiral[next_x][next_y] == 1)
+        break
+      end
+    end
+
+    x = next_x
+    y = next_y
+  end
+
+  spiral
+end
 
 # Best one
